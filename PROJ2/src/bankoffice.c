40dd2e683140b452b0908c5412afc983a0b03ef3
MOAAS <miguel.moas@gmail.com>
2019-05-07 21:16:57 +0100

    produtor consumidor

--- PROJ2/src/bankoffice.c
+++ PROJ2/src/bankoffice.c
@@ -1,23 +1,53 @@
 #include "bankoffice.h"
 
+static const int BUFFER_SIZE = 5000;
+
 static pthread_t* officeTids;
 static int* threadNums;
 static int numberOfOffices = 0;
 
 static bool* usedOffices; // partilhadooooooooo
-static pthread_mutex_t mootex = PTHREAD_MUTEX_INITIALIZER; // test :D
+
+static pthread_mutex_t queueMutex = PTHREAD_MUTEX_INITIALIZER;
+static RequestQueue* queue;
+static sem_t queueEmptySem;
+static sem_t queueFullSem;
 
 void* runBankOffice(void* officeNum) {
     int num = *(int*)officeNum;
     usedOffices[num - 1] = true;
 
 
+    for (int i = 0; i < 3; i++) {
+        printf("GOT REQUEST: %d\n", num);
+        getRequestFromQueue(num);
+    }
     // Fila de pedidos.. 
     // Acede as contas.. 
     // Repete .. 
 
     usedOffices[num - 1] = false;
     return NULL; 
+}
+
+void createQueue() {
+    queue = queue_create(BUFFER_SIZE);
+    int semValue;
+    
+    if (sem_init(&queueEmptySem, 0, BUFFER_SIZE) == -1) {
+        perror("Error creating empty sem");
+        exit(1);
+    }
+    
+    sem_getvalue(&queueEmptySem, &semValue);
+    logSyncMechSem(getSLogFD(), 0, SYNC_OP_SEM_INIT, SYNC_ROLE_PRODUCER, 0, semValue);
+
+    if (sem_init(&queueFullSem, 0, 0) == -1) {
+        perror("Error creating full sem");
+        exit(1);
+    }
+    sem_getvalue(&queueFullSem, &semValue);
+    logSyncMechSem(getSLogFD(), 0, SYNC_OP_SEM_INIT, SYNC_ROLE_PRODUCER, 0, semValue); 
 }
 
 void createBankOffices(int numOffices) {
@@ -27,6 +57,7 @@ void createBankOffices(int numOffices) {
     officeTids = malloc(numOffices * sizeof(pthread_t));
     threadNums = malloc(numOffices * sizeof(int));
     usedOffices = malloc(numOffices * sizeof(bool));
+    createQueue();
     for(int i = 0; i < numOffices; i++) {
         pthread_t tid;
         threadNums[i] = i + 1;
@@ -37,8 +68,6 @@ void createBankOffices(int numOffices) {
         logBankOfficeOpen(getSLogFD(), threadNums[i], tid);
         officeTids[i] = tid;
     }
-
-
 }
 
 void destroyBankOffices() {
@@ -50,8 +79,29 @@ void destroyBankOffices() {
     numberOfOffices = 0;
 }
 
+// Produtor
 void addRequestToQueue(tlv_request_t request) {
+    int semValue;
+    sem_getvalue(&queueEmptySem, &semValue);
+    logSyncMechSem(getSLogFD(), 0, SYNC_OP_SEM_WAIT, SYNC_ROLE_PRODUCER, request.value.header.pid, semValue);
+
+    sem_wait(&queueEmptySem); // Wait
+
+    logSyncMech(getSLogFD(), 0, SYNC_OP_MUTEX_LOCK, SYNC_ROLE_PRODUCER, request.value.header.pid);
+    pthread_mutex_lock(&queueMutex); // Lock
+    queue_push(queue, request); // Append
+    pthread_mutex_unlock(&queueMutex); // Unlock
+    logSyncMech(getSLogFD(), 0, SYNC_OP_MUTEX_UNLOCK, SYNC_ROLE_PRODUCER, request.value.header.pid);
+
+    sem_post(&queueFullSem); // Signal
+
+    sem_getvalue(&queueFullSem, &semValue);
+    logSyncMechSem(getSLogFD(), 0, SYNC_OP_SEM_POST, SYNC_ROLE_PRODUCER, request.value.header.pid, semValue);
+    
+    logRequest(getSLogFD(), 0, &request);
+
     // DEBUG
+    printf("ADD REQUEST::::\n");
     printf("Pid = %d | AccID = %d | Delay = %d | Pass = \"%s\"\n", request.value.header.pid, request.value.header.account_id, request.value.header.op_delay_ms, request.value.header.password);
     printf("Type = %d | Length = %d \n", request.type, request.length);
     if (request.type == OP_CREATE_ACCOUNT)
@@ -60,7 +110,42 @@ void addRequestToQueue(tlv_request_t request) {
         printf("Transfer: ID = %d | amount = %d \n",  request.value.transfer.account_id, request.value.transfer.amount);
 }
 
-void sendReply(tlv_reply_t reply, char* userFifoPath) {
+// Consumidor
+tlv_request_t getRequestFromQueue(int threadID) {
+    int semValue;
+    sem_getvalue(&queueFullSem, &semValue);
+    logSyncMechSem(getSLogFD(), threadID, SYNC_OP_SEM_WAIT, SYNC_ROLE_CONSUMER, 0, semValue);
+
+    sem_wait(&queueFullSem); // Wait
+
+    logSyncMech(getSLogFD(), threadID, SYNC_OP_MUTEX_LOCK, SYNC_ROLE_CONSUMER, 0);
+    pthread_mutex_lock(&queueMutex); // Lock
+    tlv_request_t request = queue_pop(queue); // Take
+    pthread_mutex_unlock(&queueMutex); // Unlock
+    logSyncMech(getSLogFD(), threadID, SYNC_OP_MUTEX_UNLOCK, SYNC_ROLE_CONSUMER, request.value.header.pid);
+
+    sem_post(&queueEmptySem); // Signal
+
+    sem_getvalue(&queueEmptySem, &semValue);
+    logSyncMechSem(getSLogFD(), threadID, SYNC_OP_SEM_POST, SYNC_ROLE_CONSUMER, request.value.header.pid, semValue);
+
+    logRequest(getSLogFD(), threadID, &request);
+
+    // DEBUG
+    printf("GOT REQUEST::::\n");
+    printf("Pid = %d | AccID = %d | Delay = %d | Pass = \"%s\"\n", request.value.header.pid, request.value.header.account_id, request.value.header.op_delay_ms, request.value.header.password);
+    printf("Type = %d | Length = %d \n", request.type, request.length);
+    if (request.type == OP_CREATE_ACCOUNT)
+        printf("Create ACC: ID = %d | Bal = %d | Pass = \"%s\"\n", request.value.create.account_id, request.value.create.balance, request.value.create.password);
+    if (request.type == OP_TRANSFER)
+        printf("Transfer: ID = %d | amount = %d \n",  request.value.transfer.account_id, request.value.transfer.amount);
+
+    
+    return request;
+}
+
+
+void sendReply(tlv_reply_t reply, char* userFifoPath, int threadID) {
     int userFifoFD = open(userFifoPath, O_WRONLY | O_NONBLOCK);
     if (userFifoFD == -1) {
         perror("Opening user Fifo");
@@ -72,5 +157,5 @@ void sendReply(tlv_reply_t reply, char* userFifoPath) {
         exit(1);
     }
     close(userFifoFD);
-    logReply(getSLogFD(), pthread_self(), &reply);
+    logReply(getSLogFD(), threadID, &reply);
 }
--- PROJ2/src/bankoffice.h
+++ PROJ2/src/bankoffice.h
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 
 #include "logfileopeners.h"
+#include "queue.h"
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 #define max(a, b) (((a) > (b)) ? (a) : (b)) 
@@ -13,3 +15,4 @@ void createBankOffices(int numOffices);
 void destroyBankOffices();
 
 void addRequestToQueue(tlv_request_t request);
+tlv_request_t getRequestFromQueue(int threadID);
