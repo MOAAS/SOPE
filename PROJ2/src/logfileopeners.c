1a8b65e0d2a39d754aee48de5ac0f0f72fe5fbcb
HenriJSantos <up201706898@fe.up.pt>
2019-05-19 20:13:44 +0100

    VERY IMPORTANT FIX

--- PROJ2/src/logfileopeners.c
+++ PROJ2/src/logfileopeners.c
@@ -4,7 +4,7 @@ static int ulogFD = 0;
 static int slogFD = 0;
 
 void openSLog() {
-    slogFD = open("slog.txt", O_WRONLY | O_CREAT | O_APPEND);
+    slogFD = open("slog.txt", O_WRONLY | O_CREAT | O_APPEND, 0777);
     if (slogFD == -1) {
         perror("Opening server log file");
         exit(1);
@@ -12,7 +12,7 @@ void openSLog() {
 }
 
 void openULog() {
-    ulogFD = open("ulog.txt", O_WRONLY | O_CREAT | O_APPEND);
+    ulogFD = open("ulog.txt", O_WRONLY | O_CREAT | O_APPEND, 0777);
     if (ulogFD == -1) {
         perror("Opening user log file");
         exit(1);
